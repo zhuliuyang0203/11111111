51eb7faf0313b69211d7bf9cea23d5288b9cd10f
Michael Render <render.michael@gmail.com>
2024-11-19 15:48:54 -0500

    Revert exception handling changes in `CanListenToFetchError`

--- dotnet/test/common/BiDi/Network/NetworkEventsTest.cs
+++ dotnet/test/common/BiDi/Network/NetworkEventsTest.cs
@@ -132,8 +132,11 @@ class NetworkEventsTest : BiDiTestFixture
 
         await using var subscription = await context.Network.OnFetchErrorAsync(tcs.SetResult);
 
-        await ((Task)context.NavigateAsync("https://not_a_valid_url.test", new() { Wait = ReadinessState.Complete }))
-            .ConfigureAwait(ConfigureAwaitOptions.SuppressThrowing);
+        try
+        {
+            await context.NavigateAsync("https://not_a_valid_url.test", new() { Wait = ReadinessState.Complete });
+        }
+        catch (Exception) { }
 
         var res = await tcs.Task.WaitAsync(TimeSpan.FromSeconds(5));
 
