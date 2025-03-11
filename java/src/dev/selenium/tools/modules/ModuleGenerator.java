47add761d0af2db214af4f2185a1d387cd4caa6b
Alex <iampopovich@gmail.com>
2025-03-11 12:40:32 +0700

    replace concat with string format + format.sh

--- java/src/dev/selenium/tools/modules/ModuleGenerator.java
+++ java/src/dev/selenium/tools/modules/ModuleGenerator.java
@@ -60,6 +60,7 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
@@ -212,7 +213,9 @@ public class ModuleGenerator {
     }
     if (result != 0) {
       throw new RuntimeException(
-          "Unable to process module:\n" + "jdeps " + String.join(" ", jdepsArgs) + "\n" + bos);
+          String.format(
+              "Unable to process module:%njdeps %s%n%s",
+              String.join(" ", jdepsArgs), bos.toString(StandardCharsets.UTF_8)));
     }
 
     AtomicReference<Path> moduleInfo = new AtomicReference<>();
